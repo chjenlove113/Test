9ccc2690b18b747303ff4a510fee21b526c0cc27
SadBoy <sadboykaka@outlook.com>
2016-09-17 21:44:34 +0700

    Create new

---
+++ i18n/i18n/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(i18n.Startup))]
+namespace i18n
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
